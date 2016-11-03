cb7a2db017b5e5444fb8952b21457d6808b2fcb1
idillia <mila.antonova@gmail.com>
2016-11-02 17:20:56 -0700

    save potantial users

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var Twit = require('twit');
 var moment = require('moment');
 var json2csv = require('json2csv');
 var config = require('./config');
+var utils = require('./utils');
 var fs = require('fs');
 var T = new Twit(config);
+var _ = require('underscore');
 
 var fields = ['id', 'screen_name', 'name', 'followers', 'creation_date', 'image_url', 'tweets'];
 var csv;
@@ -32,29 +34,10 @@ var created6MonthMore = function(account_creation_date) {
   return difference > sixMonthsInMilis ? true : false;
 } 
 
-var filterUsers = function(listOfUsers, tweetDates) {
-  var userObj = {};
-  if (listOfUsers) {
-    for (var i= 0; i <listOfUsers.length; i++) {
-      // console.log("USERS", listOfUsers[i]);
-      var userName = splitName(listOfUsers[i].name);
-      var folowersCount = followersLimit(listOfUsers[i].followers_count);
-      var creationDate = created6MonthMore(listOfUsers[i].created_at);
-      var userProfileImg = listOfUsers[i].profile_image_url;
-      if(userName && folowersCount && creationDate && userProfileImg){ 
-        console.log("GETTING list of users")
-        userObj.id = (listOfUsers[i].id).toString();
-        userObj.screen_name = listOfUsers[i].screen_name;
-        userObj.name = listOfUsers[i].name;
-        userObj.followers = listOfUsers[i].followers_count;
-        userObj.creation_date = listOfUsers[i].created_at;
-        userObj.image_url = listOfUsers[i].profile_image_url;
-        userObj.tweets = tweetDates;
-      }    
-    }
-  }
-  // console.log(userObj);
-  return userObj;
+var formatDate = function(dates) {
+  return _.map(dates, function(date) {
+    return moment(date).format("lll");
+  }); 
 }
 
 var getIdFromTweets = function(tweetList) {
@@ -65,24 +48,60 @@ var getIdFromTweets = function(tweetList) {
   return listOfUserIds;
 }
 
-var isUserActive = function(userId) {
+var isTweetsAlmostEveryday = function(tweetDates) {
+  var differenceInDays = _.difference(utils.fithteenDaysBeforeArray(), utils.formatTweetDates(tweetDates));
+  return differenceInDays.length <= 2 ? true : false;
+}
 
+var filterUsers = function(listOfUsers, tweetDates) {
+  var userObj = {};
+  if (listOfUsers) {
+    for (var i= 0; i <listOfUsers.length; i++) {
+      var userName = splitName(listOfUsers[i].name);
+      var folowersCount = followersLimit(listOfUsers[i].followers_count);
+      var creationDate = created6MonthMore(listOfUsers[i].created_at);
+      var userProfileImg = listOfUsers[i].profile_image_url;
+      var isEveryday = isTweetsAlmostEveryday(tweetDates);
+
+      if(userName && folowersCount && creationDate && userProfileImg && isEveryday){ 
+        console.log("GETTING list of users")
+        userObj.id = (listOfUsers[i].id).toString();
+        userObj.screen_name = listOfUsers[i].screen_name;
+        userObj.name = listOfUsers[i].name;
+        userObj.followers = listOfUsers[i].followers_count;
+        userObj.creation_date = listOfUsers[i].created_at;
+        userObj.image_url = listOfUsers[i].profile_image_url;
+        userObj.tweets = formatDate(tweetDates);
+      }    
+    }
+  }
+  // console.log(userObj);
+  return userObj;
 }
 
 var tweetsParams = {
   q: "work",
-  count: 8
+  count: 46
 };
 
+//B: DISPLAY LIMIT STATUS
+T.get('application/rate_limit_status')
+  .catch(function(err) {
+    console.log("limit_rate_error: ", err.stack)
+  })
+  .then(function(result) {
+    console.log("SEARCH_TWEETS_LIMIT: ",  JSON.stringify(result.data.resources.search));
+    console.log("STATUSES_USER_TIMELINE: ",JSON.stringify(result.data.resources.statuses['/statuses/user_timeline']));
+  });
+//E: DISPLAY LIMIT STATUS
 
+//B: GET TWEETS, FIND USER, FILTER
 T.get('search/tweets', tweetsParams) // Gett all tweets that has a specific key words
   .catch(function (err) {
     console.log('caught error', err.stack)
   })
   .then(function(result) {
-    // console.log('data', result.data);
     var userIds = getIdFromTweets(result.data); // Get list of usersIds
-    // console.log("usersIds", userIds);
     return userIds;
   })
   .then(function (res) {
@@ -91,7 +110,7 @@ T.get('search/tweets', tweetsParams) // Gett all tweets that has a specific key 
       // console.log("user_id: ", res[i]); 
       var timelineParams = {
         user_id: res[i],
-        count: 15
+        count: 85
       };
       T.get('statuses/user_timeline', timelineParams)
         .catch(function (err) {
@@ -99,63 +118,30 @@ T.get('search/tweets', tweetsParams) // Gett all tweets that has a specific key 
         })
         .then (function(result) {        
           var currentUserInfo = result.data[0].user;
-          // console.log("USER: ", currentUserInfo)
 
           var tweetDates = [];
           for (var i = 0; i<result.data.length; i++) {
             tweetDates.push(result.data[i].created_at);
           }
-          console.log("tweetDates", tweetDates);
-
           var filteredUsers = filterUsers([currentUserInfo], tweetDates);
           if(filteredUsers.hasOwnProperty('id')) {
             userList.push(filteredUsers);
           }
-  
         })
     }
-  })
+  });
+//B: GET TWEETS, FIND USER, FILTER  
    
-
-  setTimeout(function(){ 
-    console.log("USER_LIST2", userList);
-    csv = json2csv({data: userList, fields:fields});
-    fs.writeFile('file.csv', csv, function(err) {
-      if (err) throw err;
-      console.log('file saved');
-    });
-  }, 5000);
-  
-
-
-
-
-// T.get('users/search', userParams, userData); 
-
-
-// function userData(err,data,response) {
-//    var objData = filterUsers(data);
-//     // console.log(objData);
-//   csv = json2csv({data: objData, fields:fields});
-//   fs.writeFile('file.csv', csv, function(err) {
-//     if (err) throw err;
-//     console.log('file saved');
-//   });
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//B: SAVE TO CSV FILE
+setTimeout(function(){ 
+  // console.log("USER_LIST2", userList);
+  csv = json2csv({data: userList, fields:fields});
+  fs.writeFile('file.csv', csv, function(err) {
+    if (err) throw err;
+    console.log('file saved');
+  });
+}, 10000);
+//E: SAVE TO CSV FILE
 
 
 
---
+++ utils.js
@@ -0,0 +1,21 @@
+var moment = require('moment');
+
+
+  exports.fithteenDaysBeforeArray = function() {
+    var fithteenDaysArr = [];
+    var today = moment().format('L');
+    for (var i=0; i<15; i++) {
+      var subtractDays = moment().subtract(i,"days").format('L');
+      fithteenDaysArr.push(subtractDays);
+    }
+    return fithteenDaysArr;
+  };
+
+  exports.formatTweetDates = function(unformatedDates) {
+    var formatedTweetDatesArr = [];
+    for (var i=0; i<unformatedDates.length; i++) {
+      var formatDates = moment(unformatedDates[i]).format('L');
+      formatedTweetDatesArr.push(formatDates);
+    }
+    return formatedTweetDatesArr;
+  };
